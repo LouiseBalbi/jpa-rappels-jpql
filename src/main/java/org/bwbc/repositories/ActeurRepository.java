12ab6f52e71577f1a0f5d2429e15a80c5b8b4fcf
LouiseBalbi <67003682+LouiseBalbi@users.noreply.github.com>
2020-12-17 23:52:49 +0100

    query acteur

--- src/main/java/org/bwbc/repositories/ActeurRepository.java
+++ src/main/java/org/bwbc/repositories/ActeurRepository.java
@@ -12,21 +12,21 @@ public interface ActeurRepository extends CrudRepository<Acteur, Long> {
 	/** Recherche tous les acteurs triés par identité
 	 * @return List
 	 */
-	@Query("From Acteur")
+	@Query("From Acteur a ORDER BY a.identite")
 	List<Acteur> findActeursTriesParIdentite();
 	
 	/** Recherche tous les acteurs dont l'identité (ou partie) est passée en paramètre
 	 * @param identite identité
 	 * @return List
 	 */
-	@Query("From Acteur")
+	@Query("From Acteur a where a.identite= :identite")
 	List<Acteur> findActeursParIdentite(String identite);
 	
 	/** Recherche tous les acteurs nés l'année passée en paramètre. Les acteurs sont triés par date de naissance ascendante.
 	 * @param annee année
 	 * @return List
 	 */
-	@Query("From Acteur")
+	@Query("From Acteur a where year(a.anniversaire)= :annee ORDER BY a.anniversaire ASC")
 	List<Acteur> findActeursParAnneeNaissance(int annee);
 
 
@@ -34,21 +34,21 @@ public interface ActeurRepository extends CrudRepository<Acteur, Long> {
 	 * @param roleName nom du personnage de fiction
 	 * @return List
 	 */
-	@Query("From Acteur")
+	@Query("From Acteur a JOIN a.roles r where r.nom= :roleName")
 	List<Acteur> findActeursParRole(String roleName);
 		
 	/** Recherche tous les acteurs ayant joué dans un film paru l'année passée en paramètre.
 	 * @param annee année
 	 * @return List
 	 */
-	@Query("From Acteur")
+	@Query("From Acteur a JOIN a.roles r JOIN r.film f where f.annee= :annee")
 	List<Acteur> findActeursParFilmParuAnnee(@Param("annee") int annee);
 
 	/** Recherche tous les acteurs ayant joué dans un film produit dans le pays dont le nom est passé en paramètre
 	 * @param nom nom du pays
 	 * @return List
 	 */
-	@Query("From Acteur")
+	@Query("SELECT distinct a From Acteur a JOIN a.roles r JOIN  r.film f JOIN f.pays p where p.nom= :nom")
 	List<Acteur> findActeursParPays(String nom);
 	
 	/** Recherche tous les acteurs ayant joué dans un film paru l'année passée en paramètre et produit dans un des pays passés en paramètre
@@ -56,7 +56,7 @@ public interface ActeurRepository extends CrudRepository<Acteur, Long> {
 	 * @param annee année de parution
 	 * @return List
 	 */
-	@Query("From Acteur")
+	@Query("Select distinct a From Acteur a Join a.roles r join r.film f join f.pays p  where p.nom in (:noms) and f.annee = :annee")
 	List<Acteur> findActeursParListePaysEtAnnee(List<String> noms, int annee);
 	
 	/** Recherche tous les acteurs ayant joué dans un film réalisé par la personne dont l'identité est passée en paramètre. Seuls les films dont l'année
@@ -66,7 +66,7 @@ public interface ActeurRepository extends CrudRepository<Acteur, Long> {
 	 * @param anneeMax année max de recherche
 	 * @return List
 	 */
-	@Query("From Acteur")
+	@Query("select distinct a From Acteur a JOIN a.roles r JOIN r.film f JOIN f.realisateurs re where re.identite = :identite  and f.annee between :anneeMin and :anneeMax")
 	List<Acteur> findActeursParRealisateurEntreAnnees(String identite, int anneeMin, int anneeMax);
 	
 }
